577a5657ba9cd1357f624845985eea467784e120
Aung Kaung Myat <115795473+aungkaungmyat9504@users.noreply.github.com>
2024-06-16 00:46:02 +0700

    feat:Columncontainer and board components

---
+++ src/components/KanBanBoard.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import PlusIcon from "../assets/icons/PlusIcon";
+import { Column, Id } from "../types";
+import ColumnContainer from "./ColumnContainer";
+
+function KanBanBoard() {
+  const [columns, setColumns] = useState<Column[]>([]);
+  console.log(columns);
+
+  const createNewColumn = () => {
+    const columnToAdd: Column = {
+      id: generateId(),
+      title: `Column ${columns.length + 1}`,
+    };
+    setColumns([...columns, columnToAdd]);
+  };
+
+  const generateId = () => {
+    // Generate a random number between 0 and 10000
+    return Math.floor(Math.random() * 10001);
+  };
+
+  const deleteColumn = (id: Id) => {
+    const filteredColumns = columns.filter((col) => col.id !== id);
+    setColumns(filteredColumns);
+  };
+
+  return (
+    <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
+      <div className="m-auto flex gap-4">
+        <div className="flex gap-4">
+          {columns.map((col) => (
+            <ColumnContainer
+              key={col.id}
+              column={col}
+              deleteColumn={deleteColumn}
+            />
+          ))}
+        </div>
+        <button
+          onClick={() => {
+            createNewColumn();
+          }}
+          className="flex gap-2 h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor p-4 ring-rose-500 hover:ring-2"
+        >
+          <PlusIcon />
+          Add Column
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default KanBanBoard;
